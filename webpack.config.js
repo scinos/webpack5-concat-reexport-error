55b13f23ff6a52a5135657ef45fa75e078a7fab0
Sergio Cinos <sergio.cinos@gmail.com>
2020-11-27 14:15:36 +0100

    Add comment

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,17 @@ module.exports = {
   entry: "./index.js",
   resolve: {
     alias: {
+      // Throws error
       "@wordpress/data": path.join(
         __dirname,
         "node_modules/@wordpress/data/build"
       ),
+
+      // Works
+      // "@wordpress/data": path.join(
+      //   __dirname,
+      //   "node_modules/@wordpress/data/build-module"
+      // ),
     },
   },
   optimization: {
